e945553c98d6946b44d3a1c1a274b32fa34d7087
Evan Shrubsole <evanshrubsole@gmail.com>
2012-04-13 11:20:27 -0400

    Made the rating stripper more generic; now does all pages

--- python/rating_stripper/rating_stripper.py
+++ python/rating_stripper/rating_stripper.py
@@ -9,10 +9,15 @@ from bs4 import BeautifulSoup
 SCORE_RE = re.compile(r'score(\d\d)')
 
 LINK = \
-"https://www.rottentomatoes.com/user/id/%s/ratings?profileUserId=%s&sortby=ratingDate&pageNum=" \
+"https://www.rottentomatoes.com/user/id/%s/ratings?profileUserId=%s&sortby=" \
+"ratingDate&pageNum=" \
 % (783076641, 783076641)
 
-for i in range(1, 39):
+first_movie = None
+i = 0
+
+while True:
+    i += 1
     url = '%s%s' % (LINK, i)
     print >>sys.stderr, "opening %s" % url
     html = urllib.urlopen(url).read()
@@ -23,6 +28,8 @@ for i in range(1, 39):
 # Remove stupid "" from line 321
 
     soup = BeautifulSoup(clean_html)
+
+    n_movies = 0
 
 #    for div in soup.find_all('div', attrs={'class', 'rating_profile'}):
 #        print "%s,%s" % (div['data-title'], div['data-score'])
@@ -35,4 +42,14 @@ for i in range(1, 39):
 
         score = float(score) / 10
 
-        print "%s,%s" % (movie, score)
+        if i == 1 and n_movies == 0:
+            first_movie = movie
+        elif movie == first_movie:
+            break
+
+        print u"%s,%s" % (movie, score)
+
+        n_movies += 1
+
+    if n_movies < 10:
+        break
